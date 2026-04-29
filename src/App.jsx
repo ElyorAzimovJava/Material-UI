a060f71aa1d0cfedc816a33d9665c4eed6a97cb8
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2026-04-29 14:28:55 +0500

    The set Up has been made

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import {Button} from "@mui/material";
+
+function App() {
+  return (
+    <div>
+        <Button variant={'contained'} >Button</Button>
+    </div>
+  )
+}
+
+export default App
