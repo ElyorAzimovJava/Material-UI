dc09906e259c482c74c9cdef632a6168094ade4c
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2026-04-30 14:25:29 +0500

    Drawer has been added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,40 @@
 import {makeStyles} from "@mui/styles";
-
+import {Drawer, Typography} from "@mui/material";
+const drawerWidth = 240;
 const useStyles = makeStyles({
     page:{
         backgroundColor:'#f9f9f9 !important',
         width: '100% !important'
+    },
+    drawer:{
+      width:drawerWidth,
+    } ,
+    paperDrawer:{
+      width:drawerWidth,
+    },
+    root:{
+        display:"flex",
     }
 })
 function Layout({children}) {
     const classes = useStyles();
     return (
-        <div>
+
+        <div className={classes.root}>
+            <Drawer
+             className={classes.drawer}
+             variant="permanent"
+             anchor='left'
+             classes={{
+                 paper: classes.paperDrawer,
+             }}
+            >
+               <div>
+                   <Typography variant="h5">
+                       Ninja Notes
+                   </Typography>
+               </div>
+            </Drawer>
            <div className={classes.page}>
                {children}
            </div>
